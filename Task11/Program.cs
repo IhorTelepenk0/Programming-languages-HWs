b501807c02ab61d2c55fcfa3cdaf67ab6d475edd
IgorTelepenko <telepenko.igor2@ukr.net>
2020-10-27 19:42:11 +0200

    Добавьте файлы проекта.

---
+++ Task11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter the length: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            double aver=0, sum = 0;
+
+            //sum = SumByArray(n);
+            sum = SumByList(n);
+            Console.WriteLine("The sum of the numbers is: ");
+            Console.WriteLine(sum);
+           
+            aver = sum / n;
+            Console.WriteLine("The average is: " + aver);
+                      
+        }
+
+        static double SumByArray(int n)
+        {
+            double[] arr = new double[n];
+            double sum = 0;
+            Console.WriteLine(" enter the numbers: ");
+            for (int i=0; i < n; i++)
+            {
+                arr[i] = Convert.ToDouble(Console.ReadLine());
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+
+        static double SumByList(int n)
+        {
+            List<double> theList = new List<double>();
+            double sum = 0;
+
+            Console.WriteLine(" enter the numbers: ");
+            for (int i = 0; i < n; i++)
+            {
+                theList.Add(Convert.ToDouble(Console.ReadLine()));
+                sum += theList[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Tuple<double, double, int, double, double> results = ByList();
+            Tuple<double, double, int, double, double> results = WithoutCollections();
+            Console.WriteLine($"min = {results.Item1}, max = {results.Item2}, the count = {results.Item3}, the sum = {results.Item4}, the average = {results.Item5} ");
+
+        }
+
+        static Tuple<double,double,int,double,double> ByList()
+        {
+            List<double> theNumbers = new List<double>();
+
+            double el = 0;
+            string strel;
+            double min, max, aver, sum = 0;
+            int count = 0;
+
+            Console.WriteLine("enter the element: ");
+            strel = Console.ReadLine();
+
+            while (strel != "")
+            {
+                el = Convert.ToDouble(strel);
+                theNumbers.Add(el);
+                count++;
+                Console.WriteLine("enter the element: ");
+                strel = Console.ReadLine();
+                
+            }
+
+            
+            min = theNumbers[0];
+            max = theNumbers[0];
+            for(int i=0;i<count;i++)
+            {
+                if (theNumbers[i] > max)
+                    max = theNumbers[i];
+                if (theNumbers[i] < min)
+                    min = theNumbers[i];
+                sum += theNumbers[i];
+            }
+
+            aver = sum / count;
+
+            return new Tuple<double, double, int, double, double>(min, max, count, sum, aver);
+
+        }
+
+        static Tuple<double, double, int, double, double> WithoutCollections()
+        {
+            
+            double el = 0;
+            string strel;
+            double min, max, aver, sum = 0;
+            int count = 0;
+
+            Console.WriteLine("enter the element: ");
+            strel = Console.ReadLine();
+            min = Convert.ToDouble(strel);
+            max = Convert.ToDouble(strel);
+
+            while (strel != "")
+            {
+                el = Convert.ToDouble(strel);
+                if (max < el)
+                    max = el;
+                if (min > el)
+                    min = el;
+                sum += el;
+                count++;
+                Console.WriteLine("enter the element: ");
+                strel = Console.ReadLine();
+
+            }
+
+
+            aver = sum / count;
+
+            return new Tuple<double, double, int, double, double>(min, max, count, sum, aver);
+
+        }
+
+    }
+}

1b4039403a92a4e0206ae20d6f7ed3fe914b5789
IgorTelepenko <telepenko.igor2@ukr.net>
2020-11-04 16:22:29 +0200

    Добавьте файлы проекта.

---
+++ ArrayTasksSolving/ClassWithTasks.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace ArrayTasksSolving
+{
+    class ClassWithTasks
+    {
+
+        public static float[] Diagonal1(float[,] matrix, int n)
+        {
+            float[] res = new float[n];
+            int resInd = -1;
+            for (int i=0;i<n;i++)
+            {
+                    resInd++;
+                    res[resInd] = matrix[i,i];
+                
+            }
+        
+            return res;
+        }
+
+        public static float[] Diagonal2(float[,] matrix, int m, int n) // works actually the same as Diagonal1 (I am not sure what is the actual difference btw these taks)
+        {
+            float[] res = new float[m];
+            int resInd = -1;
+            for(int i=0;i<m;i++)
+            {
+                resInd++;
+                res[resInd] = matrix[i, resInd];
+            }
+
+            return res;
+        }
+
+        
+
+        public static float[,] Triangularize(float[,] matrix, int m, bool ifAbove)
+        {
+            float[,] res = new float[m, m];
+
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<m;j++)
+                {
+                    res[i,j] = matrix[i,j];
+                }
+            }
+
+            for(int j=0;j<m;j++)
+            {
+                for(int i=m-1;i>j;i--)
+                {   
+                    if(ifAbove == false)
+                    res[i, j] = 0;
+
+                    if (ifAbove == true)
+                        res[j, i] = 0;
+                }
+            }
+
+            return res;
+        }
+
+       /* public enum RowsOrColumns
+        {
+            rows = 0,
+            columns = 1
+        }*/
+
+        public static float[] Flatten(float[,] matrix, int m, int n) //task 12
+        {
+            float[] res = new float[m * n];
+            int resInd = -1;
+
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    resInd++;
+                    res[resInd] = matrix[i,j];
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ ArrayTasksSolving/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ArrayTasksSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,m;
+            Console.WriteLine("enter the array sizes: ");
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+
+            float[,] TwoDimArray = new float[m,n];
+            Console.WriteLine("enter the elements of the array: ");
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    TwoDimArray[i, j] = float.Parse(Console.ReadLine()); 
+                }
+            }
+
+            //float[] diagonal;
+            //diagonal = ClassWithTasks.Diagonal1(TwoDimArray, m); //task 8
+            //diagonal = ClassWithTasks.Diagonal2(TwoDimArray, m, n); // task 9
+
+            /*Console.WriteLine("the diagonal is: ");
+            for(int i=0;i<m;i++)
+            {
+                Console.WriteLine(diagonal[i] + " "); 
+            }*/ // for tasks 8 & 9
+
+
+            /*bool enum_;
+            Console.WriteLine("enter 'true' if you choose the elements above main diagonal or 'false' if under:");
+            enum_ = Convert.ToBoolean(Console.ReadLine());
+            
+            float[,] newMatrix;
+            newMatrix = ClassWithTasks.Triangularize(TwoDimArray, m, enum_);
+            Console.WriteLine("the new matrix is: ");
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    Console.WriteLine(newMatrix[i,j]);
+                }
+            }*/ //for task 11
+
+            float[] arrayFromMatr = new float[m * n];
+        
+            arrayFromMatr = ClassWithTasks.Flatten(TwoDimArray, m, n);
+            Console.WriteLine("the one-dimensional array is: ");
+
+            for(int i=0;i<m*n;i++)
+            {
+                Console.WriteLine(arrayFromMatr[i]);
+            }
+        }
+    }
+}

825a9714863b493ec7b7c12db4494a33eb0f15e7
IgorTelepenko <telepenko.igor2@ukr.net>
2020-11-08 21:30:38 +0200

    Добавьте файлы проекта.

---
+++ HW to 09.11 (Point)/Point.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW_to_09._11__Point_
+{
+
+    enum ReflectionType
+    {
+        X,
+        Y,
+        Origin
+    }
+
+    class Point
+    {
+        public double Xcoordinate;
+        public double Ycoordinate;
+
+        public Point(double x, double y)
+        {
+            Xcoordinate = x;
+            Ycoordinate = y;
+        }
+
+        public Point()
+        {
+            Xcoordinate = 1;
+            Ycoordinate = 1;
+        }
+
+        public double Distance()
+        {
+            var theDist = Math.Sqrt(Xcoordinate * Xcoordinate + Ycoordinate * Ycoordinate);
+            return theDist;
+        }
+
+        public double Distance(Point anotherPoint)
+        {
+            var xOfPoint = anotherPoint.Xcoordinate;
+            var yOfPoint = anotherPoint.Ycoordinate;
+            var theDist = Math.Sqrt((Xcoordinate - xOfPoint) * (Xcoordinate - xOfPoint) + (Ycoordinate - yOfPoint) * (Ycoordinate - yOfPoint));
+            return theDist;
+        }
+
+        public static double Distance(Point firstPoint, Point secPoint)
+        {
+            var x1 = firstPoint.Xcoordinate;
+            var y1 = firstPoint.Ycoordinate;
+            var x2 = secPoint.Xcoordinate;
+            var y2 = secPoint.Ycoordinate;
+
+            var distRes = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+            return distRes;
+        }
+
+        public override string ToString()
+        {
+            string res;
+            res = "(" + Xcoordinate.ToString() + ";" + Ycoordinate.ToString() + ")";
+            return res; 
+        }
+
+        public Point Reflect(ReflectionType reflType)
+        {
+            Point res;
+            switch(reflType)
+            {
+                case ReflectionType.X:
+                    {
+                        res = new Point(Xcoordinate, Ycoordinate * (-1));
+                        break;
+                    }
+                case ReflectionType.Y:
+                    {
+                        res = new Point(Xcoordinate * (-1), Ycoordinate);
+                        break;
+                    }
+                case ReflectionType.Origin:
+                    {
+                        res = new Point(Xcoordinate * (-1), Ycoordinate * (-1));
+                        break;
+                    }
+                default:
+                    {
+                        res = new Point();
+                        break;
+                    }
+            }
+
+            return res;
+        }
+
+    }
+}
---
+++ HW to 09.11 (Point)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW_to_09._11__Point_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //    Point p = new Point(2, 3);
+        //    Point anotherPoint = new Point(1, 0);
+        //    Point p5 = new Point(5, 5);
+        //    Point p1 = new Point();
+        //    Console.WriteLine($"dist to 0 = {p.Distance()}; dist to (1;0) = {p.Distance(anotherPoint)}; dist btw (5;5) and (1;1) = {Point.Distance(p5,p1)}");
+        //    Console.WriteLine($" my coordinates = {p.ToString()}; reflect by x = {p.Reflect(ReflectionType.X)}");
+        }
+    }
+}

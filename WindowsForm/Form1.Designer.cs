179068169a309dd66b40e97e6c88e3c67e2257fb
IgorTelepenko <telepenko.igor2@ukr.net>
2020-12-11 09:41:35 +0200

    Добавьте файлы проекта.

---
+++ WindowsForm/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.totalByNamesButton = new System.Windows.Forms.Button();
+            this.resultTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathTextBox = new System.Windows.Forms.TextBox();
+            this.readButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(257, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 35);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "By Date\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // totalByNamesButton
+            // 
+            this.totalByNamesButton.Location = new System.Drawing.Point(120, 72);
+            this.totalByNamesButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.totalByNamesButton.Name = "totalByNamesButton";
+            this.totalByNamesButton.Size = new System.Drawing.Size(130, 35);
+            this.totalByNamesButton.TabIndex = 10;
+            this.totalByNamesButton.Text = "Total by names";
+            this.totalByNamesButton.UseVisualStyleBackColor = true;
+            this.totalByNamesButton.Click += new System.EventHandler(this.totalByNamesButton_Click);
+            // 
+            // resultTextBox
+            // 
+            this.resultTextBox.Location = new System.Drawing.Point(13, 131);
+            this.resultTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.resultTextBox.Multiline = true;
+            this.resultTextBox.Name = "resultTextBox";
+            this.resultTextBox.Size = new System.Drawing.Size(763, 305);
+            this.resultTextBox.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "File path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pathTextBox
+            // 
+            this.pathTextBox.Location = new System.Drawing.Point(13, 36);
+            this.pathTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pathTextBox.Name = "pathTextBox";
+            this.pathTextBox.Size = new System.Drawing.Size(763, 26);
+            this.pathTextBox.TabIndex = 7;
+            this.pathTextBox.Text = "D:\\help folder if needed\\invoices.txt";
+            // 
+            // readButton
+            // 
+            this.readButton.Location = new System.Drawing.Point(17, 72);
+            this.readButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.readButton.Name = "readButton";
+            this.readButton.Size = new System.Drawing.Size(95, 35);
+            this.readButton.TabIndex = 6;
+            this.readButton.Text = "Read file";
+            this.readButton.UseVisualStyleBackColor = true;
+            this.readButton.Click += new System.EventHandler(this.readButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.totalByNamesButton);
+            this.Controls.Add(this.resultTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pathTextBox);
+            this.Controls.Add(this.readButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button totalByNamesButton;
+        private System.Windows.Forms.TextBox resultTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathTextBox;
+        private System.Windows.Forms.Button readButton;
+    }
+}
+
---
+++ WindowsForm/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1() => InitializeComponent();
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void readButton_Click(object sender, EventArgs e)
+        {
+            resultTextBox.Text = string.Join("", ReadFile());
+        }
+
+        private void totalByNamesButton_Click(object sender, EventArgs e)
+        {
+            //Old school way
+            // var result = new Dictionary<string, decimal>();
+            //
+            // foreach (var invoice in invoices)
+            // {
+            //     if (!result.ContainsKey(invoice.Name))
+            //     {
+            //         result[invoice.Name] = invoice.Amount;
+            //     }
+            //     else
+            //     {
+            //         result[invoice.Name] += invoice.Amount;
+            //     }
+            // }
+            //
+            // resultTextBox.Text = "";
+            // foreach (var entry in result.OrderBy(x=>x.Key))
+            // {
+            //     resultTextBox.Text += $"{entry.Key}: {entry.Value}\r\n";
+            // }
+
+            //Modern approach:
+            var result = ListOfInvoices()
+                .GroupBy(x => x.Name)
+                .Select(x => $"{x.Key}: {x.Sum(y => y.Amount)}");
+
+            resultTextBox.Text = string.Join("\r\n", result);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var result = ListOfInvoices()
+
+                .OrderByDescending(x => x.DateTime)
+                .GroupBy(z => z.Name)
+                .Select(z => $"{z.Key}: {z.Sum(y => y.Amount)}");
+
+
+            resultTextBox.Text = $"FROM NEWEST TO OLDEST:\r\n{string.Join("\r\n", result)}";
+        }
+        private IEnumerable<string> ReadFile()
+        {
+            var path = pathTextBox.Text;
+
+            if (!File.Exists(path))
+            {
+                MessageBox.Show("File does not exist. Cannot proceed");
+                return null;
+            }
+
+            var lines = File.ReadAllLines(path).Skip(1);
+            return lines;
+        }
+
+        private List<Invoice> ListOfInvoices()
+        {
+            var lines = ReadFile();
+
+            var invoices = new List<Invoice>();
+            foreach (var line in lines)
+            {
+                var split = line.Split('\t');
+                var name = split[0];
+                var date = DateTime.ParseExact(split[1], "yyyy-MM-dd", null);
+                var amount = Convert.ToDecimal(split[2].Replace(",", CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator)); // 120.21
+
+                var invoice = new Invoice(name, date, amount);
+                invoices.Add(invoice);
+            }
+            return invoices;
+        }
+    }
+    public class Invoice
+    {
+        public string Name { get; }
+        public DateTime DateTime { get; }
+        public decimal Amount { get; }
+
+        public Invoice(string name, DateTime dateTime, decimal amount)
+        {
+            Name = name;
+            DateTime = dateTime;
+            Amount = amount;
+        }
+    }
+}
